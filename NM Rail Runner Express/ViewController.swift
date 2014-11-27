b2c0b6ee34851deab3e6e3c7359c41cc2638ccfd
Chris Hughes <cfhughes@Croissant.local>
2014-11-27 12:54:12 -0700

    Initial Commit

---
+++ NM Rail Runner Express/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NM Rail Runner Express
+//
+//  Created by Chris Hughes on 11/27/14.
+//  Copyright (c) 2014 Chris Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
